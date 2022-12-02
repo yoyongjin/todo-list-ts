0f99ada35632fac1798958f7a4b30d29f484a8ba
yoyongjin <yongjinalize@gmail.com>
2022-12-02 17:24:59 +0900

    install: redux, react-redux, @types/react-redux packages intalled

--- src/components/atoms/TodoInput.tsx
+++ src/components/atoms/TodoInput.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
   flex-grow: 1;
 `;
 
+// 여기서 AddTodoForm으로 input value 전달
 const TodoInput = () => {
-  return <Input />;
+  const [enteredTodo, setEnteredTodo] = useState("");
+
+  const todoInputChangeHandler = (e: any) => {
+    setEnteredTodo(e.target.value);
+  };
+
+  return (
+    <Input
+      type="text"
+      id="addTodo"
+      onChange={todoInputChangeHandler}
+      value={enteredTodo}
+    />
+  );
 };
 
 export default TodoInput;
